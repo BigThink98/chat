fd5a955e55091814aed8aa95bb8d0872f86b4b6e
BigThink98 <46754467+BigThink98@users.noreply.github.com>
2021-04-12 12:20:41 -0400

    Initial commit

---
+++ src/chat/ChatClientTest.java
@@ -0,0 +1,14 @@
+package chat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatClientTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
